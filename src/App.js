4b9e1c61c0fdb4c07abe39e3922e9752385df66d
Aidana Zhumakova <zhumakova99@gmail.com>
2021-09-25 15:48:49 +0600

    Downloaded dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Layout from "./components/Navigation/Layout/Layout";
+import ContactBuilder from "./containers/ContactBuilder/ContactBuilder";
+import {BrowserRouter} from "react-router-dom";
 
 const App = () => {
     return (
-        <div>
-          
-        </div>
+        <BrowserRouter>
+            <Layout>
+                <ContactBuilder/>
+            </Layout>
+        </BrowserRouter>
     );
 };
 
